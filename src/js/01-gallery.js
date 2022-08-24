d706fb05dd78394c8aef184c0b9fd4b68aa57c14
Solod Sergey <Solod098@gmail.com>
2022-08-24 15:16:26 +0300

    1st ok

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
+import simpleLightbox from "simplelightbox";
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from "./gallery-items";
 // Change code below this line
 
-console.log(galleryItems);
+const SimpleLightboxGallery = {
+  gallery: document.querySelector(".gallery"),
+  createGalleryMarkUpFrom(galleryItems) {
+    return galleryItems
+      .map(
+        ({ preview, original, description }) => `<li class="gallery__li">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li> `
+      )
+      .join("");
+  },
+};
+
+const newGallery = SimpleLightboxGallery.createGalleryMarkUpFrom(galleryItems);
+// новая галлерея === запуск функции создания разметки
+SimpleLightboxGallery.gallery.insertAdjacentHTML("beforeend", newGallery);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionType: "alt",
+  captionsData: "alt",
+  captionDelay: 200,
+  showCounter: false,
+  maxZoom: 2,
+  scrollZoomFactor: 0.1,
+});
+// создаем lightbox (экземпляр объекта SimpleLightbox из библиотеки которую мы подключили) + изменяем некоторые параметры
