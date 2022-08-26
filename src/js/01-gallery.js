fb160072016dc7fe27117f6d1e56a814e7b9fd67
Solod Sergey <Solod098@gmail.com>
2022-08-26 17:17:33 +0300

    task 3 ready

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,7 @@
 import simpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+import "../css/common.css";
+import "../css/01-gallery.css";
 // Add imports above this line
 import { galleryItems } from "./gallery-items";
 // Change code below this line
